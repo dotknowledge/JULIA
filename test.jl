8636ccf0bdf37c10264832b9c15de249a4f83ba9
dotknowledge <dotknowledge@users.noreply.github.com>
2022-09-15 15:34:07 +0200

    updated funtions

--- test.jl
+++ test.jl
@@ -1,18 +1,15 @@
 # Testing Julia
-using Statistics, Random, Distributions, LinearAlgebra, StatsPlots
+using Statistics, Random, Distributions, LinearAlgebra, StatsPlots, StatsBase
 #Variables
 
 # Returns random D(n) "dice roll"
 roll_die(n::Integer) = rand(1:n)
 
-function get_roll(k,n)
-    rolls = []
-    for r in 1:k
-        push!(rolls, roll_die(n))
-    end
-    rolls
+function get_roll(k,die=6)
+    rand(1:die, k)
 end
 
+get_roll(6)
 
 #roll = get_roll(3, 6)
 #attributeRolls = [sum(get_roll(2,8))+2 for r in 1:6#]
@@ -22,9 +19,11 @@ end
 #histogram(altAttribs)
 
 function get_attribs()
-    sum(rand(2:6, (3,6)), dims=1)
+    sum(rand(1:6, (3,6)), dims=1)
 end
 
 attributes = vec(get_attribs())
 character = Dict(zip(("str", "dex", "con", "int", "wis", "cha"), attributes))
 println(character)
+
+rs = [sum(get_roll(3,6)) for _ in 1:12]; println(sort(rs, rev=true))
